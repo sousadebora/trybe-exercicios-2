b551000482e948c3d630557a09bff843ebfc05ac
Débora Mendonça <deboradesousa25@gmail.com>
2021-11-07 19:59:38 -0300

    Exercicio 3

---
+++ fundamentos/bloco4-introduçao-a-javascript-e-logica-de-programaçao/dia1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,11 @@
+const a = 8;
+const b = 10;
+const c = 12;
+
+if (a > b && a > c){
+    console.log("O maior número é a");
+} else if (b > a && b > c) {
+  console.log("O maior número é b");
+} else {
+  console.log("O maior número é c");
+}
