3558abbb0ba5e78fb5c5f89b38b1df69c582ca6d
Débora Mendonça <deboradesousa25@gmail.com>
2021-11-08 18:32:35 -0300

    Exercicios 1, 2 e 3 'Agora a prática'

---
+++ fundamentos/bloco4-introduçao-a-javascript-e-logica-de-programaçao/dia2-array-e-loop-for/exercicio3.js
@@ -0,0 +1,9 @@
+//3.Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+    total += numbers[index];
+}
+let media = total / numbers.length;
+console.log(media);
